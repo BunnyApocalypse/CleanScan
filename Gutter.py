e81e06472c567c430b9146eb5d85f290db4d7b36
kingc3 <cking@fwparker.org>
2016-07-21 11:25:50 -0500

    Adding corner support

--- Gutter.py
+++ Gutter.py
@@ -4,37 +4,23 @@ import numpy as np
 
 img1 = cv2.imread("TestImages/textscan.jpg")
 #kernel = cv2.getStructuringElement(cv2.MORPH_CLOSE, (11, 11))
-def gutter(img):
+def gutter(img1):
     grayImg = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((11,11),np.uint8)
     grayImg = cv2.GaussianBlur(grayImg, (111, 111), 1)
     grayImg = cv2.erode(grayImg, kernel, iterations=1)
     res, grayImg = cv2.threshold(grayImg, 175, 255, cv2.THRESH_BINARY)
-    im2, contrs, hier = cv2.findContours(grayImg,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img1, contrs, -1, (0,255,0), 3)
+    im2, contrs, hier = cv2.findContours(grayImg,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    #for c in contrs:
+    # cv2.drawContours(img1, contrs, -1, (0,0,0), -1 )
     #cv2.drawContours(img1,contour,-1,(255,0,0),-1)
-    cannyImg = cv2.Canny(grayImg, 100, 200)
-
-    lines = cv2.HoughLinesP(cannyImg, 1, np.pi/180,
-
-                            threshold = 5,
-
-                            minLineLength = 300, maxLineGap = 70 )
-    for lineSet in lines:
-
-        for line in lineSet:
-
-            cv2.line(img1, (line[0], line[1]), (line[2], line[3]),
-
-                     (255, 255, 0))
-
-    goodFeats = cv2.goodFeaturesToTrack(grayImg, 200, 0.455, 5)
-
-    for x in goodFeats:
-        cv2.circle(img1, (x[0,0],x[0,1]),3,(0,0,255),-1)
+    (h,w,d) = img1.shape
+    mask = np.zeros((h,w,1),np.uint8)
+    cv2.drawContours(mask, contrs, -1, (255,255,255), -1)
+    finalImg = cv2.bitwise_and(img1,img1, mask=mask)
+    cv2.imshow("fixed image", finalImg)
     cv2.imshow("HoughLines", img1)
     cv2.imshow("greyscale", grayImg)
+    cv2.imshow("mask",mask)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
 gutter(img1)
